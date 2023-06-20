719770285aa8ed721e5844879466d74e2409dc50
arslan-rt <arslan.muhammad@rolustech.com>
2023-06-20 12:27:54 +0500

    After delete the item from list of Todo's

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import { toDo } from "../../toDo";
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-
-  constructor() { }
+  todos:toDo[];
+  constructor() {  
+    this.todos = [
+      {
+        sno:1,
+        title: "It's Ahmad here",
+        desc: "Developer"
+      },
+      {
+        sno:2,
+        title: "It's Saad here",
+        desc: "HR"
+      },
+      {
+        sno:3,
+        title: "It's Sam here",
+        desc: "Manager"
+      }
+    ]
+  }
 
   ngOnInit() {
   }
 
+  deleteTodo(todo:toDo){
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
+  }
 }
